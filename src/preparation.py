ecfa4b74167b911613d8e5ccb91286b3cfdaff43
Tatiana <tanya.living.sto@gmail.com>
2022-09-09 13:37:25 +0300

    refactored code (pylint, black, code logic)

--- src/preparation.py
+++ src/preparation.py
@@ -1,69 +1,58 @@
-import base64
-import colorsys
-import io
 import os
-import pickle
-import pprint
-import re
-import string
-import sys
 import warnings
-from collections import Counter
-from operator import itemgetter
-from random import Random
-from xml.sax import saxutils
 import yaml
-
-import matplotlib
-import matplotlib.lines as mlines
-import matplotlib.pyplot as plt
 import nltk
-import numpy as np
 import pandas as pd
-import seaborn as sns
-from bs4 import BeautifulSoup
-import nltk
-nltk.download('omw-1.4')
-from nltk.tokenize import sent_tokenize, word_tokenize
-from PIL import Image, ImageColor, ImageDraw, ImageFilter, ImageFont
-
 from sklearn.model_selection import StratifiedShuffleSplit
-from functions import *
+from functions import pipe
 
-nltk.download('wordnet')
-nltk.download('stopwords')
-nltk.download('punkt')
+nltk.download("omw-1.4")
 
-warnings.filterwarnings('ignore')
+nltk.download("wordnet")
+nltk.download("stopwords")
+nltk.download("punkt")
 
-config_path = os.path.join('/home/asdf/prj/MLFlow/config/config.yaml')
-config = yaml.safe_load(open(config_path))['preparation']
+warnings.filterwarnings("ignore")
 
+config_path = os.path.join("./config/config.yaml")
+config = yaml.safe_load(open(config_path))["preparation"]
+
+
+# for filename in os.listdir("/MLFlow/data/"):
+#    with open(os.path.join("/MLFlow/data/", filename), 'r') as f:
+#        text = f.read()
+#        print(filename)
+#        print(text)
+
+# print(os.listdir(f'{os.path.join(os.getcwd(), "data")}'))
+# print(config['data_path'])
 ## Read data
+data = pd.read_csv(config["data_path"], sep=",", header=0, nrows=100)
+# data = pd.read_csv('/home/asdf/prj/MLFlow/data/LargeMovieReviewDataset.csv', sep=',', header=0, nrows = 1000)
+# print(data.sentiment.value_counts())
 
-data = pd.read_csv(config['data_path'])
 
 ## Splitting
-
 X = data.review
 y = data.sentiment
 
-sss_train_test = StratifiedShuffleSplit(n_splits=config['n_splits'], test_size=config['test_size'], random_state=config['random_state'])
+sss_train_test = StratifiedShuffleSplit(
+    n_splits=config["n_splits"],
+    test_size=config["test_size"],
+    random_state=config["random_state"],
+)
+# for train_index, test_index in sss_train_test.split(X, y):
+#     print("TRAIN:", train_index, "TEST:", test_index)
 train_index, test_index = list(sss_train_test.split(X, y))[0]
 train = data.loc[train_index]
 test = data.loc[test_index]
 
 # Data prepocessing
-
 train_pipe = pipe(data.loc[train_index])
-
-train_pipe.to_csv('/home/asdf/prj/MLFlow/out/train_pipe.csv', sep=";")
+train_pipe.to_csv("/MLFlow/data/train_pipe.csv", sep=";")
 
 # test pipe
-
 test_pipe = pipe(test)
 
 # Save test processing
-
-test_pipe.to_csv('/home/asdf/prj/MLFlow/out/test_pipe.csv', sep=";")
-
+test_pipe.to_csv("/MLFlow/data/test_pipe.csv", sep=";")
