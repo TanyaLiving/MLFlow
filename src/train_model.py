3a6a71552ae8c3037be203a113e0a5bc4565bed1
Tatiana <tanya.living.sto@gmail.com>
2022-09-14 12:09:28 +0300

    remove unnecessary files

--- src/train_model.py
+++ src/train_model.py
@@ -1,9 +1,4 @@
-
-
-# from psutil import STATUS_LOCKED
-import os
-from tokenize import Triple
-from functions import *
+from datetime import datetime
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -12,28 +7,31 @@ from sklearn.linear_model import (
 )
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import (
-    GridSearchCV,
     StratifiedShuffleSplit,
-    cross_val_score
+    cross_val_score,
 )
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
-from functools import partial
 import mlflow
-from mlflow.tracking import MlflowClient
-from urllib.parse import urlparse
 import yaml
-import os
-from hyperopt import space_eval
-import hyperopt.pyll
+import hyperopt
+from hyperopt import hp
+from hyperopt import space_eval, STATUS_OK, fmin, tpe
+from functions import (
+    model_result,
+    conf_matrix,
+    plot_conf_matrix,
+    feature_importance,
+    r_a_score,
+)
 
-config_path = os.path.join('/home/asdf/prj/MLFlow/config/config.yaml')
-config = yaml.safe_load(open(config_path))['train_model']
+config = yaml.safe_load(open("./config/config.yaml"))["train_model"]
 
 ## Preprocessed data
 
-test_preprocessed = pd.read_csv(config['data_path_test'], sep=';')
+test_preprocessed = pd.read_csv(config["data_path_test"], sep=";")
 
-train_preprocessed = pd.read_csv(config['data_path_train'], sep=';')
+train_preprocessed = pd.read_csv(config["data_path_train"], sep=";")
 
 # Splitting
 
@@ -43,131 +41,154 @@ train_y = train_preprocessed.sentiment.array
 test_X = test_preprocessed.review.array
 test_y = test_preprocessed.sentiment.array
 
-class_names = ['negative', 'positive']
+class_names = ["negative", "positive"]
 
-mlflow.set_tracking_uri("http://localhost:5000/")
-mlflow.set_experiment(experiment_name = config["experiment_name"])
-
-## Logistic regression
-
-# Baseline
-
-params = {"random_state": config['random_state'], 'solver': config['solver']}
-
-cv = StratifiedShuffleSplit(n_splits=config['n_splits'], test_size=config['test_size'], random_state=config['random_state'])
-
-my_pipeline = Pipeline([
-    ('vectorizer', TfidfVectorizer()),
-    ('clf', LogisticRegression(random_state = params['random_state'], solver = params['solver']))
-])
-
-# Train
-
-with mlflow.start_run(run_name=config["run_name"], tags = {config['tag_name_key']: config['tag_name_value']}):
-    model_result(my_pipeline,
-                train_X, 
-                train_y,
-                train_X, 
-                )
-                
-    ROC_AUC_train_LR = round(roc_auc_score(train_y, my_pipeline.predict_proba(train_X)[:, 1]), 2)
-    
-    mlflow.log_params(params)
-    mlflow.log_metric("ROC_AUC_train_LR", ROC_AUC_train_LR)
-
-# Cross-validaion
-
-    ROC_AUC_cv_LR = Cr_Val(my_pipeline, train_X, train_y, cv)
-    mlflow.log_metric("ROC_AUC_CV_LR", ROC_AUC_cv_LR)
+mlflow.set_tracking_uri("http://mlflow_web_servise:5000/")
+exp_id = mlflow.set_experiment(experiment_name=config["experiment_name"])
 
 
-# Test
-    model_result(my_pipeline,
-             train_X,
-             train_y,
-             test_X,  
-             )
-
-    ROC_AUC_test_LR = round(roc_auc_score(test_y, my_pipeline.predict_proba(test_X)[:, 1]), 2)
-    mlflow.log_metric("ROC_AUC_test_LR", ROC_AUC_test_LR)
+# Preparation
+RANDOM_STATE = 42
+cv = StratifiedShuffleSplit(
+    n_splits=config["n_splits"],
+    test_size=config["test_size"],
+    random_state=RANDOM_STATE,
+)
 
 # Search
 
-    search_space = {
-        'clf__penalty': hp.choice('clf__penalty', ['l1', 'l2']),
-        'clf__C': hp.uniform(label='clf__C', low = 0.0001, high = 10.0)
-    }
+if config["model"] == "log_reg":
+    my_pipeline = Pipeline(
+        [
+            ("vectorizer", TfidfVectorizer()),
+            ("clf", LogisticRegression(random_state=RANDOM_STATE)),
+        ]
+    )
 
-    def objective(x):
-        params = {
-                'clf__penalty': x['clf__penalty'],
-                'clf__C': x['clf__C'],
-        }
+elif config["model"] == "MultinomialNB":
+    my_pipeline = Pipeline(
+        [
+            ("tfidf", TfidfVectorizer()),
+            ("clf", MultinomialNB()),
+        ]
+    )
 
-        estimator = my_pipeline.set_params(**params).fit(train_X, train_y)
+with mlflow.start_run(
+    run_name=f'PARENT_{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}'
+):
 
-        score = cross_val_score(estimator = estimator,
-                                X = train_X,
-                                y = train_y,
-                                scoring = 'roc_auc',
-                                cv = 10,
-                                error_score='raise').mean()
-        print(f"AUC {score}, params {params}")
-        return {'loss': 1-score.mean(), 
-                'params': params,
-                'status': STATUS_OK}
-    trials = Trials()
-    
-    
-    max_evals = 1
+    search_space = hp.choice(
+        "classifier_type",
+        [
+            {
+                "type": "naive_bayes",
+                "clf__alpha": hp.uniform(
+                    "clf__alpha",
+                    low=config["low_alpha_NB"],
+                    high=config["high_alpha_NB"],
+                ),
+            },
+            {
+                "type": "log_reg",
+                "clf__penalty": hp.choice(
+                    "clf__penalty", config["clf__penalty_choice"]
+                ),
+                "clf__C": hp.uniform(
+                    label="clf__C", low=config["low_C"], high=config["high_C"]
+                ),
+            },
+        ],
+    )
 
+    def objective(x_var):
+
+        with mlflow.start_run(
+            run_name=f'CHILD_{datetime.now().strftime("%H:%M:%S")}',
+            experiment_id=exp_id.experiment_id,
+            nested=True,
+        ) as child_run:
+            classifier_type = config["model"]
+            if classifier_type == "log_reg":
+                estimator = my_pipeline.set_params(clf__solver=config["solver"]).fit(
+                    train_X, train_y
+                )
+            elif classifier_type == "naive_bayes":
+                estimator = my_pipeline.fit(train_X, train_y)
+            score = cross_val_score(
+                estimator=estimator,
+                X=train_X,
+                y=train_y,
+                scoring=config["scoring"],
+                cv=config["cross_val_score_cv"],
+                error_score="raise",
+            ).mean()
+            mlflow.log_params(x_var)
+            print(estimator)
+            mlflow.sklearn.log_model(estimator, "model")
+            print('mlflow.sklearn.log_model(estimator, "model")')
+
+            mlflow.log_metric("ROC_AUC_TRAIN", score)
+            print(x_var)
+
+            return {"loss": 1 - score.mean(), "params": x_var, "status": STATUS_OK}
 
     best = fmin(
-            fn = objective,
-            space = search_space,
-            algo = tpe.suggest,
-            max_evals = max_evals,
-            trials = trials,
-            rstate = np.random.default_rng(1),
-            show_progressbar = True
+        fn=objective,
+        space=search_space,
+        algo=tpe.suggest,
+        max_evals=config["max_evals"],
+        rstate=np.random.default_rng(RANDOM_STATE),
+        show_progressbar=True,
     )
-    # for i in trials.results:
-    #     print(i['params']['clf__penalty'])
-    dict_trials = {i : trials.results[i]['params']['clf__penalty'] for i in range(max_evals)}
-    # print(dict_trials)
-    print()
-    print('trials.results')
-    print(trials.results)
-    
+
     best_params = space_eval(search_space, best)
+    print(best_params)
+
+    # Best model
+
+    if best_params["type"] == "log_reg":
+        my_pipeline_tuned = Pipeline(
+            [
+                ("vectorizer", TfidfVectorizer()),
+                (
+                    "clf",
+                    LogisticRegression(
+                        C=best_params["clf__C"],
+                        penalty=best_params["clf__penalty"],
+                        solver=config["solver"],
+                        random_state=RANDOM_STATE,
+                    ),
+                ),
+            ]
+        )
+
+    elif best_params["type"] == "naive_bayes":
+        my_pipeline_tuned = Pipeline(
+            [
+                ("vectorizer", TfidfVectorizer()),
+                ("clf", MultinomialNB(alpha=best_params["clf__alpha"])),
+            ]
+        )
+
+    best_result = model_result(
+        my_pipeline_tuned,
+        train_X,
+        train_y,
+        test_X,
+    )
+    mlflow.sklearn.log_model(my_pipeline_tuned, "model")
+
+    ROC_AUC_test_LR_tuned = round(
+        roc_auc_score(test_y, my_pipeline_tuned.predict_proba(test_X)[:, 1]), 2
+    )
+    mlflow.log_metric("ROC_AUC_TEST", ROC_AUC_test_LR_tuned)
+
     mlflow.log_params(best_params)
 
-# # Best model
-
-    my_pipeline_tuned = Pipeline([
-        ('vectorizer', TfidfVectorizer()),
-        ('clf', LogisticRegression(C = best_params['clf__C'], 
-                                penalty = best_params['clf__penalty'],
-                                solver =  config['solver'],
-                                random_state =  config['random_state']))
-    ])
-
-    model_result(my_pipeline_tuned,
-                train_X,
-                train_y,
-                test_X,  
-                )
-    mlflow.sklearn.log_model(my_pipeline_tuned, 'model')
-
-    ROC_AUC_test_LR_tuned = round(roc_auc_score(test_y, my_pipeline_tuned.predict_proba(test_X)[:, 1]), 2)
-
-    mlflow.log_metric("ROC_AUC_test_LR_tuned", ROC_AUC_test_LR_tuned)
-
-    conf_matr = conf_matrix(test_y, class_names, model_result(my_pipeline_tuned, train_X, train_y, test_X))
-
-    plot_conf_matrix('Logistic Regression', class_names, conf_matr)
-
-# Feature importance plots
-
-    feature_importance(my_pipeline_tuned, 20)
+    conf_matr = conf_matrix(test_y, class_names, best_result)
+    plot_conf_matrix(config["experiment_name"], class_names, conf_matr)
     r_a_score(my_pipeline_tuned, train_X, train_y, test_X, test_y)
+
+    # Feature importance plots
+    if best_params["type"] == "log_reg":
+        feature_importance(my_pipeline_tuned, config["fi_plot_n_features"])
